a42b18784b0de85f5c717328d2e0ecc6c7bd2419
johnatanlopes <johnatanlopes@gmail.com>
2019-01-22 23:14:12 -0200

    Padrão Strategy utilizando exemplo de impostos

---
+++ Strategy/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require "Orcamento.php";
+require 'Imposto.php';
+require "CalculadoraDeImpostos.php";
+require 'ICMS.php';
+require 'ISS.php';
+require 'KCV.php';
+
+$reforma = new Orcamento(500);
+$calculadora = new CalculadoraDeImpostos();
+echo "Valor do ICMS: " . $calculadora->calcula($reforma, new ICMS()) . "<br>";
+echo "Valor do ISS: " . $calculadora->calcula($reforma, new ISS()) . "<br>";
+echo "Valor do KCV: " . $calculadora->calcula($reforma, new KCV());
